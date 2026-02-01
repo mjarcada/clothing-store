6a2fb89cb1c51d4eb6cd9c7bc9bbd5b8379e6324
mjarcada <masoomim@arcada.fi>
2026-02-01 15:23:44 +0200

    Add product_id to GET products

--- app/products.py
+++ app/products.py
@@ -2,6 +2,7 @@ from app.conn import get_conn
 
 def get_products():
     with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("SELECT p.name as product_name, c.name as category_name, p.price, p.stock FROM products p INNER JOIN categories c ON p.category_id = c.category_id ORDER BY p.name;")
+        cur.execute("SELECT p.product_id, p.name as product_name, c.name as category_name, p.price, p.stock FROM products p INNER JOIN categories c ON p.category_id = c.category_id ORDER BY p.name;")
 
-        return cur.fetchall()# GET /products
+        return cur.fetchall()
+      
