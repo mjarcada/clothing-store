00a3a3c7f1a26408819b5778ec9c1236d26eda4d
mjarcada <masoomim@arcada.fi>
2026-02-01 15:04:23 +0200

    Refine products

--- app/products.py
+++ app/products.py
@@ -2,5 +2,6 @@ from app.conn import get_conn
 
 def get_products():
     with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("SELECT * FROM products ORDER BY product_id;")
+        cur.execute("SELECT products.name as product_name, categories.name as category_name, products.price, products.stock FROM products INNER JOIN categories ON products.category_id = categories.category_id ORDER BY products.name;")
+
         return cur.fetchall()# GET /products
